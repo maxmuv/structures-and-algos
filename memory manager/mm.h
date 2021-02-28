513f6c2d7d1204fb8f1fb406b6f59f520e9ff5b3
maxmuv <mmaksimenko534@gmail.com>
2021-02-28 04:18:13 +0300

    Memory manager

--- memory manager/mm.h
+++ memory manager/mm.h
@@ -52,7 +52,10 @@ namespace lab618
                 m_pBlocks = newBlock();
                 m_pCurrentBlk = m_pBlocks;
                 m_pCurrentBlk->usedCount++;
-                m_pCurrentBlk->firstFreeIndex++;
+                if (m_blkSize == 1)
+                    m_pCurrentBlk->firstFreeIndex = -1;
+                else
+                    m_pCurrentBlk->firstFreeIndex++;
                 memset(reinterpret_cast<void*>(m_pCurrentBlk->pdata), 0, sizeof(T));
                 ::new(reinterpret_cast<void*>(m_pCurrentBlk->pdata)) T;
                 return m_pCurrentBlk->pdata;
@@ -62,13 +65,16 @@ namespace lab618
                 m_pCurrentBlk->pnext = newBlock();
                 m_pCurrentBlk = m_pCurrentBlk->pnext;
                 m_pCurrentBlk->usedCount++;
-                m_pCurrentBlk->firstFreeIndex++;
+                if (m_blkSize == 1)
+                    m_pCurrentBlk->firstFreeIndex = -1;
+                else
+                    m_pCurrentBlk->firstFreeIndex++;
                 memset(reinterpret_cast<void*>(m_pCurrentBlk->pdata), 0, sizeof(T));
                 ::new(reinterpret_cast<void*>(m_pCurrentBlk->pdata)) T;
                 return m_pCurrentBlk->pdata;
             }
             T* object = &(m_pCurrentBlk->pdata[m_pCurrentBlk->firstFreeIndex]);
-            int *newFirstFreeIndex = reinterpret_cast<int *>(&(m_pCurrentBlk->pdata[m_pCurrentBlk->firstFreeIndex]));
+            int *newFirstFreeIndex = reinterpret_cast<int *>(&(m_pCurrentBlk->pdata[m_pCurrentBlk->firstFreeIndex]));            
             m_pCurrentBlk->firstFreeIndex = *newFirstFreeIndex;
             m_pCurrentBlk->usedCount++;
             memset(reinterpret_cast<void*>(object), 0, sizeof(T));
@@ -79,7 +85,53 @@ namespace lab618
         // ?????????? ??????? ? ?????????
         bool deleteObject(T* p)
         {
-            return true;
+            if (m_pBlocks == 0)
+                return false;
+            block *p_tmpblock = m_pBlocks;
+            block *p_prevblock = 0;
+            int find = -1;
+            for (int i = 0; i < m_blkSize; i++) {
+                if (p == &(p_tmpblock->pdata[i]))
+                    find = i;
+            }
+            while ((find == -1) && (p_tmpblock->pnext != 0)) {
+                p_prevblock = p_tmpblock;
+                p_tmpblock = p_tmpblock->pnext;
+                for (int i = 0; i < m_blkSize; i++) {
+                    if (p == &(p_tmpblock->pdata[i]))
+                        find = i;
+                }
+            }
+            if (find != -1) {
+                int freeindex = p_tmpblock->firstFreeIndex;
+                if (freeindex != -1)
+                    while (*reinterpret_cast<int *>(&(p_tmpblock->pdata[freeindex])) != -1) {
+                        if (freeindex == find)
+                            return false;
+                        freeindex = *reinterpret_cast<int *>(&(p_tmpblock->pdata[freeindex]));
+                    }
+                if (freeindex == find)
+                    return false;
+                (&(p_tmpblock->pdata[find]))->~T();
+                memset(reinterpret_cast<void*>(&(p_tmpblock->pdata[find])), 0, sizeof(T));
+                if (freeindex != -1)
+                    *reinterpret_cast<int *>(&(p_tmpblock->pdata[freeindex])) = find;
+                else
+                    p_tmpblock->firstFreeIndex = find;
+                *reinterpret_cast<int *>(&(p_tmpblock->pdata[find])) = -1;
+                p_tmpblock->usedCount--;
+                if (p_tmpblock->usedCount == 0){
+                    if (p_prevblock != 0)
+                        p_prevblock->pnext = p_tmpblock->pnext;
+                    else
+                        m_pBlocks = p_tmpblock->pnext;
+                    if (m_pCurrentBlk == p_tmpblock)
+                        m_pCurrentBlk = p_prevblock;
+                    deleteBlock(p_tmpblock);
+                }
+                return true;
+            }
+            return false;
         }
 
         // ??????? ??????, ??????? ?? m_isDeleteElementsOnDestruct
@@ -96,6 +148,8 @@ namespace lab618
                 p_nextblock = p_block->pnext;
             }
             deleteBlock(p_block);
+            m_pBlocks = 0;
+            m_pCurrentBlk = 0;
         }
     private:
 
@@ -140,7 +194,7 @@ namespace lab618
                 delete p;
                 return;
             }
-            if (p->firstFreeIndex != -1)
+            if (p->firstFreeIndex != 0)
                 throw std::runtime_error("Error in deleteBlock().\nBlock isn't empty.\n");
             free(p->pdata);
             delete p;
--- memory manager/test.cpp
+++ memory manager/test.cpp
@@ -11,9 +11,16 @@ struct TestStruct
     std::string key;
     std::string value1;
     std::string value2;
+    TestStruct *next;
 };
 
-typedef lab618::CMemoryManager<TestStruct> TestMemoryManger;
+struct TestListStruct
+{
+    TestStruct *p_head;
+    TestStruct *p_end;
+};
+
+typedef lab618::CMemoryManager<TestStruct> TestMemoryManager;
 
 static std::string makeRandomString(int minL = 7, int maxL = 14)
 {
@@ -35,17 +42,57 @@ static void generate(TestStruct *pts)
     pts->key = makeRandomString();
     pts->value1 = makeRandomString();
     pts->value2 = makeRandomString();
+    pts->next = 0;
 }
 
 void TestMMFunction()
 {
-    TestMemoryManger TestMM(20, true);
+    TestListStruct TestL;
+    TestMemoryManager TestTrueMM(20, true);
     for (int i = 0; i < ELEMENTS_COUNT; i++)
     {
-        TestStruct *ts = TestMM.newObject();
+        TestStruct *ts = TestTrueMM.newObject();
         generate(ts);
     }
-    TestMM.clear();
+    TestTrueMM.clear();
+    TestMemoryManager TestFalseMM(1, false);
+    TestStruct *p_teststruct = TestFalseMM.newObject();
+    generate(p_teststruct);
+    TestL.p_head = p_teststruct;
+    TestL.p_end = p_teststruct;
+    for (int i = 1; i < ELEMENTS_COUNT; i++){
+        TestL.p_end->next = TestFalseMM.newObject();
+        TestL.p_end = TestL.p_end->next;
+        generate(TestL.p_end);
+    }
+    p_teststruct = TestL.p_head;
+    TestStruct *p_tmpnext;
+    for (int i = 0; i < ELEMENTS_COUNT; i++) {
+        p_tmpnext = p_teststruct->next;
+        assert(TestFalseMM.deleteObject(p_teststruct) == true);
+        p_teststruct = p_tmpnext;
+    }
+    TestFalseMM.clear();
+    TestMemoryManager TestTrueMM2(10, true);
+    p_teststruct = TestTrueMM2.newObject();
+    generate(p_teststruct);
+    TestL.p_head = p_teststruct;
+    TestL.p_end = p_teststruct;
+    for (int i = 1; i < ELEMENTS_COUNT; i++){
+        TestL.p_end->next = TestTrueMM2.newObject();
+        TestL.p_end = TestL.p_end->next;
+        generate(TestL.p_end);
+    }
+    p_teststruct = TestL.p_head;
+    for (int i = 0; i < 19; i++) {
+        p_teststruct = p_teststruct->next;
+    }
+    for (int i = 20; i < ELEMENTS_COUNT; i++) {
+        p_tmpnext = p_teststruct->next;
+        assert(TestTrueMM2.deleteObject(p_teststruct) == true);
+        p_teststruct = p_tmpnext;
+    }
+    TestTrueMM2.clear();
 }
 
 int main()
